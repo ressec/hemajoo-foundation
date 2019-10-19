7431f317a9254db2928f6de918b960d1aff872a8
Christophe Resse <christophe.resse@ch.ibm.com>
2019-10-20 00:16:29 +0200

    Implement a base set of exception and annotation entities.

---
+++ foundation-common/src/main/java/com/ressec/hemajoo/foundation/common/annotation/Experimental.java
@@ -0,0 +1,33 @@
+/*
+ * (C) Copyright Hemajoo Systems Inc.  2019 - All Rights Reserved
+ * -----------------------------------------------------------------------------------------------
+ * All information contained herein is, and remains the property of
+ * Hemajoo Inc. and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Hemajoo Inc. and its
+ * suppliers and may be covered by U.S. and Foreign Patents, patents
+ * in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained from
+ * Hemajoo Systems Inc.
+ * -----------------------------------------------------------------------------------------------
+ */
+package com.ressec.hemajoo.foundation.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Types implementing this interface are experimental and can be changed or
+ * removed at any time by the development team without any prior notice.
+ * <br>
+ *
+ * @author <a href="mailto:christophe.resse@gmail.com">Christophe Resse</a>
+ * @version 1.0.0
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Experimental
+{
+    // Marker interface
+}
---
+++ foundation-common/src/main/java/com/ressec/hemajoo/foundation/common/exception/AbstractUncheckedException.java
@@ -0,0 +1,74 @@
+/*
+ * (C) Copyright Hemajoo Systems Inc.  2019 - All Rights Reserved
+ * -----------------------------------------------------------------------------------------------
+ * All information contained herein is, and remains the property of
+ * Hemajoo Inc. and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Hemajoo Inc. and its
+ * suppliers and may be covered by U.S. and Foreign Patents, patents
+ * in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained from
+ * Hemajoo Systems Inc.
+ * -----------------------------------------------------------------------------------------------
+ */
+package com.ressec.hemajoo.foundation.common.exception;
+
+/**
+ * An abstract implementation of an unchecked exception.
+ * <br><br>
+ * Unchecked runtime exceptions represent conditions that, generally speaking, reflect errors in program's logic and
+ * cannot be reasonably recovered from at run time. Unchecked exceptions are subclasses of RuntimeException,
+ * and are usually implemented using InvalidArgumentException, NullPointerException, or
+ * IllegalStateException a method is not obliged to establish a policy for the unchecked exceptions thrown by
+ * its implementation (and they almost always do not do so).
+ *
+ * @author <a href="mailto:christophe.resse@gmail.com">Christophe Resse</a>
+ * @version 1.0.0
+ */
+public abstract class AbstractUncheckedException extends RuntimeException
+{
+    /**
+     * Default serialization identifier.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Creates a new unchecked empty exception.
+     */
+    public AbstractUncheckedException()
+    {
+        super();
+    }
+
+    /**
+     * Creates a new unchecked exception based on a parent exception.
+     *
+     * @param exception Parent exception.
+     */
+    public AbstractUncheckedException(final Exception exception)
+    {
+        super(exception);
+    }
+
+    /**
+     * Creates a new unchecked exception based on a message.
+     *
+     * @param message Message of the exception.
+     */
+    public AbstractUncheckedException(final String message)
+    {
+        super(message);
+    }
+
+    /**
+     * Creates a new unchecked exception based on a message and a parent exception.
+     *
+     * @param message   Message of the exception.
+     * @param exception Parent exception.
+     */
+    public AbstractUncheckedException(final String message, final Exception exception)
+    {
+        super(message, exception);
+    }
+}
